eee1cacd6dbad113d87352120cccf5bd151ede56
ipk7218 <ialexandr7218@gmail.com>
2025-08-20 01:11:41 +0300

    changed code + renamed

---
+++ Task1_1.py
@@ -0,0 +1,10 @@
+# Make up an algorithm
+# If the entered number is greater than 7, then print “Hello” 
+
+def HelloGreater(num):
+	if num > 7:
+		print('Hello')
+
+#test
+print('Task1.1')
+HelloGreater(8) # Hello
