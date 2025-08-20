eca728f047b64beabf30366496a50a174381a84f
ipk7218 <ialexandr7218@gmail.com>
2025-08-21 00:38:13 +0300

    code is ready

--- Task1.py
+++ Task1.py
@@ -1,48 +1,59 @@
-def HelloGreater():
+def hello_greater():
 	"""
 	Make up an algorithm
 	If the entered number is greater than 7, then print “Hello”. 
 
 	"""
-	print('Enter your number')
-	num = int(input())
+	num = int(input('Enter number: '))
 	if num > 7:
 		print('Hello')
 
-def HelloJohn():
+def hello_john():
 	"""
 	If the entered name matches “John”, then output “Hello, John”, 
 	if not, then output "There is no such name". 
 
 	"""
-	print('Type your text')
-	text = input()
+	text = input('Enter text: ')
 	if text == 'John':
 		print('Hello, John')
 	else:
 		print('There is no such name')
 
-def Array3mod():
-    """There is a numeric array at the input, it is necessary to output array elements that are multiples of 3."""
-    print('Type your number(s)')
-    nums = input().split(", ")
+def array_3_mod():
+    """
+	There is a numeric array at the input, it is necessary to 
+	output array elements that are multiples of 3.
+	
+	"""
+    nums = input('Type your number(s): ').split(", ")
     result_num = []
     for i in range(len(nums)):
         if int(nums[i]) % 3 == 0:
             result_num.append(int(nums[i]))
     print(result_num)
     
-def SelectTask():
-	'''This Selector Choosing Task1 from 1 to 3'''
+def select_task():
+	'''
+	This Selector Choosing Task1 from 1 to 3. 
+	Also Checking Exception by Value cases.
+	
+	'''
 	print('Select Task Number')
 	num = int(input())
 	if num == 1:
-		HelloGreater()
+		try:
+			hello_greater()
+		except ValueError:
+			print('Value should be int')
 	elif num == 2:
-		HelloJohn()
+			hello_john()
 	elif num == 3:
-		Array3mod()
+		try:
+			array_3_mod()
+		except ValueError:
+			print('Value should be int')
 	else:
 		return None
 		
-SelectTask()
+select_task()
