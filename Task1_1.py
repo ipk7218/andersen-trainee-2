a1c2c304574f922f65930cf8dcba5076fd82059c
ipk7218 <ialexandr7218@gmail.com>
2025-08-20 21:25:21 +0300

    Changed the code for the result of the Task

--- Task1_1.py
+++ Task1_1.py
@@ -1,10 +1,45 @@
-# Make up an algorithm
-# If the entered number is greater than 7, then print “Hello” 
+def HelloGreater():
+	"""Make up an algorithm
+	If the entered number is greater than 7, then print “Hello”. 
 
-def HelloGreater(num):
+	"""
+	print('Enter your number')
+	num = int(input())
 	if num > 7:
 		print('Hello')
 
-#test
-print('Task1.1')
-HelloGreater(8) # Hello
+
+def HelloJohn():
+	"""#If the entered name matches “John”, then output “Hello, John”, 
+	   if not, then output "There is no such name". 
+	
+	"""
+	print('Type your text')
+	text = input()
+	if text == 'John':
+		print('Hello, John')
+	else:
+		print('There is no such name')
+
+
+def Array3mod():
+    """There is a numeric array at the input, it is necessary to output array elements that are multiples of 3."""
+    print('Type your number(s)')
+    nums = input().split(", ")
+    result_num = []
+    for i in range(len(nums)):
+        if int(nums[i]) % 3 == 0:
+            result_num.append(int(nums[i]))
+    print(result_num)
+    
+def tester():
+	print('Select Task Number')
+	num = int(input())
+	if num == 1:
+		HelloGreater()
+	elif num == 2:
+		HelloJohn()
+	elif num == 3:
+		Array3mod()
+		
+tester()
