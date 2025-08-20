89fea4d1e5d97d0d2332fa96896906cc485394b2
ipk7218 <ialexandr7218@gmail.com>
2025-08-20 21:45:57 +0300

    changed comment, renamed func tester as SelectTask and added comments

--- Task1.py
+++ Task1.py
@@ -1,18 +1,20 @@
 def HelloGreater():
-	"""Make up an algorithm
+	"""
+	Make up an algorithm
 	If the entered number is greater than 7, then print “Hello”. 
 
 	"""
 	print('Enter your number')
 	num = int(input())
-	if num > 7:
+	if num < 7:
+
 		print('Hello')
 
-
 def HelloJohn():
-	"""#If the entered name matches “John”, then output “Hello, John”, 
-	   if not, then output "There is no such name". 
-	
+	"""
+	If the entered name matches “John”, then output “Hello, John”, 
+	if not, then output "There is no such name". 
+
 	"""
 	print('Type your text')
 	text = input()
@@ -20,7 +22,6 @@ def HelloJohn():
 		print('Hello, John')
 	else:
 		print('There is no such name')
-
 
 def Array3mod():
     """There is a numeric array at the input, it is necessary to output array elements that are multiples of 3."""
@@ -32,7 +33,8 @@ def Array3mod():
             result_num.append(int(nums[i]))
     print(result_num)
     
-def tester():
+def SelectTask():
+	'''This Selector Choose Task 1 from 1 to 3'''
 	print('Select Task Number')
 	num = int(input())
 	if num == 1:
@@ -41,5 +43,7 @@ def tester():
 		HelloJohn()
 	elif num == 3:
 		Array3mod()
+	else:
+		return None
 		
-tester()
+SelectTask()
